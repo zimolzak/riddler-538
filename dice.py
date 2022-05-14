86a21a1987ca1e3d75e99c5058189d3d1d7e23bd
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-05-14 11:31:54 -0500

    Do the final absorbing MC calculations!

--- dice.py
+++ dice.py
@@ -117,9 +117,10 @@ if __name__ == '__main__':
     n_wins = np.sum(win_loss)
     dur_win = durations[win_loss == 1]
     dur_loss = durations[win_loss == 0]
+    p_numeric = np.mean(win_loss)
 
     print()
-    print("P =", np.mean(win_loss))  # P = 0.45132 at 200 k
+    print("P =", p_numeric)  # P = 0.45132 at 200 k
     print()
 
     print("N trials =", n_sims_setting)
@@ -133,13 +134,13 @@ if __name__ == '__main__':
     # build up tensor by tiling
     dw = np.multiply(durations, win_loss)
     dl = np.multiply(durations, 1 - win_loss)
-    t = np.stack((
+    tensor = np.stack((
         np.broadcast_to(dw, (np.max(durations), n_sims_setting)),
         np.broadcast_to(dl, (np.max(durations), n_sims_setting))
     ))
     print("Shape of intermediate tensor for 'extra credit,' or eventually")
     print("doing histogram of duration stratified by win/loss:")
-    print(t.shape)
+    print(tensor.shape)
     print("Expect (2, max dur, n sims)")
 
     tr_rowsum = tr.sum(axis=1).reshape((tr.shape[0], 1))
@@ -147,7 +148,7 @@ if __name__ == '__main__':
     tr_normalized = tr / tr_allsum
     print()
 
-    print("Transition matrix:")
+    print("Transition matrix (MC estimate, extra row for init):")
     # print(tr, "\n")
     # print(tr_allsum, "\n")
     print(tr_normalized, "\n")
@@ -163,14 +164,19 @@ if __name__ == '__main__':
         uniq, dupe = parse_dice(list(four_dice_tup))
         score = len(uniq)
         score_4[score] += 1
-    print("Closed-form transition vector for init or 3 dups")
+    print("Closed-form transition vector for init or 3 dups, 1 unique")
     print("(same as row 1 or 5 of Transition matrix):")
     print(score_4, "/ 256 =")
-    print([x / 256 for x in score_4])
+    cf = [x / 256 for x in score_4]
+    print(cf)
+    print()
+
+
+
 
     """Absorbing Markov chain:
-    t = 3 transient states
-    r = 2 absorbing states
+    t = 3 transient states {1, 2, 3}
+    r = 2 absorbing states {0, 4}
     Q is 3x3
     R is 3x2
     
@@ -181,3 +187,55 @@ if __name__ == '__main__':
     0 0 0 1 0
     0 0 0 0 1
     """
+
+    print("\n\n\n# Absorbing Markov chain calculations\n")
+
+    r = 2
+    t = ndi + 1 - r
+    Q = np.array([
+        [cf[1], cf[2], cf[3]],  # transition from 1 to something, denom 256
+        [1/8, 5/8, 0],  # from 2 to something
+        [0, 0, 0]  # hopefully works
+    ])
+    R = np.array([
+        [cf[0], cf[4]],
+        [1/8, 1/8],
+        [0, 1]
+    ])
+    P = np.vstack((
+        np.hstack((Q, R)),
+        np.hstack((np.zeros((r, t)), np.identity(r)))
+    ))
+    print("P = determined from closed form dice probabilities =")
+    print(P)
+    print()
+
+    # fundamental matrix
+    N = np.linalg.inv(np.identity(t) - Q)
+    print("N = (I - Q)^-1 = ")
+    print(N)
+    print()
+
+    # Expected steps starting at transient i
+    print("vector t = N * ones = ")
+    t_bold = np.matmul(N, np.ones((t, 1)))
+    print(t_bold)
+    print()
+
+    # Absorbing probabilities (start in trans state i, land in abs state j)
+    # Remember, the whole game starts basically in trans state 1 (same as 1 unique, 3 dup)
+    # So Pr(win) = B_11 = 0.45, agreeing fine with numeric monte carlo estimate.
+    # Pr(loss) = B_10 = 0.55.
+    # Here, Python would call it B[0, 1] but I am calling it B_11
+    B = np.matmul(N, R)
+    print("B = N * R =")
+    print(B)
+    print()
+
+    print("Reminder, numeric monte carlo estimate was:")
+    print(p_numeric)
+    print()
+
+    print("FINAL ANSWER!")
+    print(B[0, 1])
+    print()
